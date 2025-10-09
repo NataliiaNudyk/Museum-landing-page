3c264de2e4ffcd3205253331fbb9b35c7d57a958
Nataliia Nudyk <nataliianudyk@gmail.com>
2025-10-09 13:53:42 +0300

    make build

---
+++ index.c99adec0.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0},grabCursor:!0,breakpoints:{498:{slidesPerView:2.1,spaceBetween:20},0:{slidesPerView:1.07,spaceBetween:16}}});
+//# sourceMappingURL=index.c99adec0.js.map
